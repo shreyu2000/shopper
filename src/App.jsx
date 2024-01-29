da7a6415717be815edee55aed3ab2713bc1ce0db
shubhamghawas <123919711+shubhamghawas@users.noreply.github.com>
2024-01-29 15:32:27 +0530

    added admin

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Admin from "./Pages/Admin/Admin"
+import Navbar from "./components/Navbar/Navbar"
+
+function App() {
+  return (
+    <div>
+    <Navbar/>
+    <Admin/>
+    </div>
+  )
+}
+
+export default App;
