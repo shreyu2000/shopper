45b8c4987eeb655180087c603875cd7d52b58064
shubhamghawas <123919711+shubhamghawas@users.noreply.github.com>
2024-01-30 10:37:37 +0530

    updated addproduct

--- src/components/AddProduct/AddProduct.jsx
+++ src/components/AddProduct/AddProduct.jsx
@@ -7,7 +7,6 @@ const AddProduct = () => {
   const [image, setImage] = useState(null);
   const [productDetails, setProductDetails] = useState({
     name: "",
-    image: "",
     category: "women",
     new_price: "",
     old_price: "",
@@ -72,8 +71,49 @@ const AddProduct = () => {
 
   return (
     <div className="add-product">
-      {/* Input fields for product details */}
-      {/* ... */}
+      <div className="addproduct-itemfield">
+        <p>Product Title</p>
+        <input
+          value={productDetails.name}
+          onChange={changeHandler}
+          type="text"
+          name="name"
+          placeholder="Type Here"
+        />
+      </div>
+      <div className="addproduct-price">
+        <div className="addproduct-itemfields">
+          <p>Price</p>
+          <input
+            value={productDetails.old_price}
+            onChange={changeHandler}
+            type="text"
+            name="old_price"
+          />
+        </div>
+        <div className="addproduct-itemfields">
+          <p>Offer Price</p>
+          <input
+            value={productDetails.new_price}
+            onChange={changeHandler}
+            type="text"
+            name="new_price"
+          />
+        </div>
+      </div>
+      <div className="addproduct-itemfield">
+        <p>Product Category</p>
+        <select
+          value={productDetails.category}
+          onChange={changeHandler}
+          name="category"
+          className="add-product-selector"
+        >
+          <option value="women">Women</option>
+          <option value="men">Men</option>
+          <option value="kid">Kid</option>
+        </select>
+      </div>
       <div className="addproduct-itemfield">
         <label htmlFor="file-input">
           <img
